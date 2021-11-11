9a3ac75ac35f88975513e23ee1a35726dc4518f0
Matt Luker <jamesmattluker@gmail.com>
2021-11-11 14:22:03 -0700

    add script to pass test 2 for AgeCalculator

--- src/js/age-calculator.js
+++ src/js/age-calculator.js
@@ -2,7 +2,10 @@ export default class AgeCalculator {
   constructor(userInputAge, userInputLifeExpectancy) {
     this.age = userInputAge;
     this.lifeExpectancyEarth = userInputLifeExpectancy;    
-  }  
+  }
+  marsAge() {
+    this.marsAge = this.age / .24;
+  }
 }
 
 
