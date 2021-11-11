d04f87139d5bebbec1586f926adaa0b6f667bb20
Matt Luker <jamesmattluker@gmail.com>
2021-11-11 13:04:25 -0700

    refactor AgeCalculator and remove unneeded tests

--- __tests__/AgeCalculator.test.js
+++ __tests__/AgeCalculator.test.js
@@ -8,27 +8,11 @@ describe('AgeCalculator', () => {
 
 
   test('should correctly create a AgeCalculator object with six key value pairs', () => {
-    expect(myAgeCalculator.mercuryAge).toEqual(42);
-    expect(myAgeCalculator.venusAge).toEqual(42);
-    expect(myAgeCalculator.marsAge).toEqual(42);
-    expect(myAgeCalculator.jupiterAge).toEqual(42);
+    expect(myAgeCalculator.mercuryAge).toEqual(175);
+    expect(myAgeCalculator.venusAge).toEqual(67.74193548387098);
+    expect(myAgeCalculator.marsAge).toEqual(22.340425531914896);
+    expect(myAgeCalculator.jupiterAge).toEqual(3.5413153456998314);
     expect(myAgeCalculator.lifeExpectancy).toEqual(85);
     expect(myAgeCalculator.lifeExpectancySurpassedBy).toEqual(85);
-  });
-  test('should take user inputted value, multiply it  it by .24 and assign that value to mercuryAge', () => {
-    myAgeCalculator.calcMercuryAge();
-    expect(myAgeCalculator.mercuryAge).toEqual(175);
-  });
-  test('should take user inputted value, divide it by .62 and assign that value to venusAge', () => {
-    myAgeCalculator.calcVenusAge();
-    expect(myAgeCalculator.venusAge).toEqual(67.74193548387098);
-  });
-  test('should take user inputted value, divide it by 1.88 and assign that value to marsAge', () => {
-    myAgeCalculator.calcMarsAge();
-    expect(myAgeCalculator.marsAge).toEqual(22.340425531914896);
-  });
-  test('should take user inputted value, divide it by 11.86 and assign that value to jupiterAge', () => {
-    myAgeCalculator.calcJupiterAge();
-    expect(myAgeCalculator.jupiterAge).toEqual(3.5413153456998314);
   });
 });
--- src/js/age-calculator.js
+++ src/js/age-calculator.js
@@ -1,22 +1,10 @@
 export default class AgeCalculator {
   constructor(userInputAge, userInputLifeExpectancy) {
-    this.mercuryAge = userInputAge;
-    this.venusAge = userInputAge;
-    this.marsAge = userInputAge;
-    this.jupiterAge = userInputAge;
+    this.mercuryAge = userInputAge / .24;
+    this.venusAge = userInputAge / .62;
+    this.marsAge = userInputAge / 1.88;
+    this.jupiterAge = userInputAge / 11.86;
     this.lifeExpectancy = userInputLifeExpectancy;
     this.lifeExpectancySurpassedBy = userInputLifeExpectancy;
-  }
-  calcMercuryAge() {
-    this.mercuryAge /= .24;
-  }
-  calcVenusAge() {
-    this.venusAge /= .62;
-  }
-  calcMarsAge() {
-    this.marsAge /= 1.88;
-  }
-  calcJupiterAge() {
-    this.jupiterAge /= 11.86
-  }
+  }  
 }
