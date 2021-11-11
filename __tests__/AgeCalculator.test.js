634e2ffb916c78716da11d849633d65719220c12
Matt Luker <jamesmattluker@gmail.com>
2021-11-11 14:43:47 -0700

    add test 5 for AgeCalculator

--- __tests__/AgeCalculator.test.js
+++ __tests__/AgeCalculator.test.js
@@ -20,4 +20,7 @@ describe('AgeCalculator', () => {
   test('should take this.age and divide it by 1.88 and return that value as a new marsAge key value', () => {    
     expect(myAgeCalculator.marsAge()).toEqual(22.340425531914896);
   });
+  test('should take this.age and divide it by 11.86 and return that value as a new jupiterAge key value', () => {    
+    expect(myAgeCalculator.jupiterAge()).toEqual(22.340425531914896);
+  });
 });
