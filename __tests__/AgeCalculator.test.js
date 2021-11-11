24fdcb7fc5f8ef357950286c71e49b02d77770d8
Matt Luker <jamesmattluker@gmail.com>
2021-11-11 15:28:34 -0700

    rewrite test 7 for AgeCalculator

--- __tests__/AgeCalculator.test.js
+++ __tests__/AgeCalculator.test.js
@@ -26,8 +26,7 @@ describe('AgeCalculator', () => {
   test('should take this.lifeExpectancyEarth and see if it is greater than or equal to this.userInputAge', () => {
     expect(myAgeCalculator.olderOrNot()).toEqual(true);
   });
-  test('should return the sum of this.lifeExpectancyEarth - this.age as the new yearsLeft key value', () => {
-    myAgeCalculator.yearsLeft();
-    expect(myAgeCalculator.yearsLeft).toEqual(43);
+  test('should return the sum of this.lifeExpectancyEarth - this.age', () => {
+    expect(myAgeCalculator.yearsLeft()).toEqual(43);
   });
 });
