3d19322cbeb4e3a0ededa084cfc2bfdfe3055f73
Matt Luker <jamesmattluker@gmail.com>
2021-11-11 15:04:48 -0700

    write script to pass test 6 for AgeCalculator

--- src/js/age-calculator.js
+++ src/js/age-calculator.js
@@ -7,13 +7,20 @@ export default class AgeCalculator {
     return this.age / .24;
   }
   venusAge() {
-    return this.age / .62
+    return this.age / .62;
   }
   marsAge() {
-    return this.age / 1.88
+    return this.age / 1.88;
   }
   jupiterAge() {
-    return this.age / 11.86
+    return this.age / 11.86;
+  }
+  olderOrNot() {
+    if (this.lifeExpectancyEarth >= this.age) {
+      return true
+    } else {
+      return false
+    }
   }
 }
 
