9fb601b3608a66e2a80e892131f7c7c64836d653
Matt Luker <jamesmattluker@gmail.com>
2021-11-11 10:28:34 -0700

    add test 1 for AgeCalculator

---
+++ __tests__/AgeCalculator.test.js
@@ -0,0 +1,14 @@
+import AgeCalculator from './../src/js/age-calculator.js';
+
+describe('AgeCalculator', () => {
+
+  test('should correctly create a AgeCalculator object with six key value pairs', () => {
+    const myAgeCalculator = new AgeCalculator(42);
+    expect(myAgeCalculator.mercuryAge).toEqual(42);
+    expect(myAgeCalculator.venusAge).toEqual(42);
+    expect(myAgeCalculator.marsAge).toEqual(42);
+    expect(myAgeCalculator.jupiterAge).toEqual(42);
+    expect(myAgeCalculator.lifeExpectancy).toEqual(42);
+    expect(myAgeCalculator.lifeExpectancySurpassBy).toEqual(42);
+  });  
+});
