11505b2300e49662cdb0270b98a1f981d8e51623
Matt Luker <jamesmattluker@gmail.com>
2021-11-11 11:09:38 -0700

    write test 2 for AgeCalculator

--- __tests__/AgeCalculator.test.js
+++ __tests__/AgeCalculator.test.js
@@ -1,14 +1,18 @@
-import AgeCalculator from './../src/js/AgeCalculator.js';
+import AgeCalculator from './../src/js/age-calculator.js';
 
 describe('AgeCalculator', () => {
 
   test('should correctly create a AgeCalculator object with six key value pairs', () => {
-    const myAgeCalculator = new AgeCalculator(42);
+    let myAgeCalculator = new AgeCalculator(42);
     expect(myAgeCalculator.mercuryAge).toEqual(42);
     expect(myAgeCalculator.venusAge).toEqual(42);
     expect(myAgeCalculator.marsAge).toEqual(42);
     expect(myAgeCalculator.jupiterAge).toEqual(42);
     expect(myAgeCalculator.lifeExpectancy).toEqual(42);
-    expect(myAgeCalculator.lifeExpectancySurpassBy).toEqual(42);
-  });  
+    expect(myAgeCalculator.lifeExpectancySurpassedBy).toEqual(42);
+  });
+  test('should take user inputted integer, divide it by .24 and assign that value to AgeCalculator.mercuryAge', () => {
+    let myAgeCalculator = new AgeCalculator(42);
+    expect(myAgeCalculator.mercuryAge).toEqual(175);
+  });
 });
