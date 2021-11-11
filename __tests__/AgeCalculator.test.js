fbd1bb42b849363acd132d8d50f448e6dc58814c
Matt Luker <jamesmattluker@gmail.com>
2021-11-11 11:34:35 -0700

    add script to pass AgeCalculator test 2

--- __tests__/AgeCalculator.test.js
+++ __tests__/AgeCalculator.test.js
@@ -1,18 +1,22 @@
 import AgeCalculator from './../src/js/age-calculator.js';
 
 describe('AgeCalculator', () => {
+  let myAgeCalculator;
+  beforeEach(() => {
+    myAgeCalculator = new AgeCalculator(42, 85);
+  });
+
 
   test('should correctly create a AgeCalculator object with six key value pairs', () => {
-    let myAgeCalculator = new AgeCalculator(42);
     expect(myAgeCalculator.mercuryAge).toEqual(42);
     expect(myAgeCalculator.venusAge).toEqual(42);
     expect(myAgeCalculator.marsAge).toEqual(42);
     expect(myAgeCalculator.jupiterAge).toEqual(42);
-    expect(myAgeCalculator.lifeExpectancy).toEqual(42);
-    expect(myAgeCalculator.lifeExpectancySurpassedBy).toEqual(42);
+    expect(myAgeCalculator.lifeExpectancy).toEqual(85);
+    expect(myAgeCalculator.lifeExpectancySurpassedBy).toEqual(85);
   });
-  test('should take user inputted integer, divide it by .24 and assign that value to AgeCalculator.mercuryAge', () => {
-    let myAgeCalculator = new AgeCalculator(42);
+  test('should take user inputted integer, divide it by .24 and assign that value to mercuryAge', () => {
+    myAgeCalculator.calcMercuryAge();
     expect(myAgeCalculator.mercuryAge).toEqual(175);
   });
 });
