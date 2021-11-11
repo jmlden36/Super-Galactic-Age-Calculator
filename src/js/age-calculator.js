2803da566fa7a695679a5490be5c653f6b336ec5
Matt Luker <jamesmattluker@gmail.com>
2021-11-11 10:17:29 -0700

    write test 1 for AgeCalculator

---
+++ src/js/age-calculator.js
@@ -0,0 +1,10 @@
+export default class AgeCalculator {
+  constructor(userInputAge) {
+    this.mercuryAge = userInputAge;
+    this.venusAge = userInputAge;
+    this.marsAge = userInputAge;
+    this.jupiterAge = userInputAge;
+    this.lifeExpectancy = userInputAge;
+    this.lifeExpectancySurpassedBy = userInputAge;
+  }
+}
